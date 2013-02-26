ef77fb88dc088f29d523cb8537c0e8f9e464b786
Bernd Giesecke <bernd@giesecke.tk>
2013-02-26 10:40:08 +0900

    Added ISO country code for Serbia (rs)

--- src/android/romstats/Utilities.java
+++ src/android/romstats/Utilities.java
@@ -278,6 +278,7 @@ public class Utilities {
 	            "qa", "Qatar",
 	            "re", "Reunion (French)",
 	            "ro", "Romania",
+	            "rs", "Serbia",
 	            "ru", "Russian Federation",
 	            "rw", "Rwanda",
 	            "sa", "Saudi Arabia",
